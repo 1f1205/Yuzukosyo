c504d30c5680086b43355f58d183684c57d40058
alk-tnk <alk.tnk@gmail.com>
2013-11-22 20:23:29 +0900

    add .gitignore

--- crawler/ArticleModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-class ArticleModel {
-    public $title;
-    public $link;
-    public $publishedDate;
-    public $content;
-    public $categories;
-    public $rssId;
-}
--- crawler/Main.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function __autoload($class_name) {
-    require_once $class_name . '.php';
-}
-
-$crawler = new Crawler();
-$crawler->exec();
