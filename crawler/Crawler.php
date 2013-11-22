c74b46f4ae67895c4c1c2a2b4fcbd2a205558e65
alk-tnk <alk.tnk@gmail.com>
2013-11-22 20:59:07 +0900

    接続するURLをフォーマット化

--- crawler/Crawler.php
+++ crawler/Crawler.php
@@ -1,5 +1,9 @@
 <?php
 class Crawler {
+    // Google Feed APIのURL
+    const GOOGLE_FEED_API = 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=%d&q=%s';
+    // 取得するページ数
+    const PAGE_NUM = 20;
 
     /**
      * 実行ファイルから呼び出す 
@@ -15,11 +19,25 @@ class Crawler {
         $this->fileGet($urls);
     }
 
+    /**
+     * jsonファイルを取得する 
+     *
+     * @param mixed $url 
+     * @access private
+     * @return void
+     */
     private function fileGet($url) {
-        $this->parse(file_get_contents($url));
+        $this->parseJson(file_get_contents($url));
     }
-
-    private function parse($content) {
+    
+    /**
+     * jsonファイルをパースし、配列を返す
+     *
+     * @param mixed $content 
+     * @access private
+     * @return void
+     */
+    private function parseJson($content) {
         $decode = json_decode($content)->responseData->feed;
 
         // 記事をパース
@@ -30,6 +48,6 @@ class Crawler {
     }
 
     private function selectRssTable() {
-        return "https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://blog.livedoor.jp/nanjstu/index.rdf&num=20";
+        return sprintf(self::GOOGLE_FEED_API, self::PAGE_NUM, "http://blog.livedoor.jp/nanjstu/index.rdf");
     }
 }
